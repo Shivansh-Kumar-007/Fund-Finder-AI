432cd160eb9011c065b59b5cba3cb12390275054
Shivansh_ <shivanshkumar994@gmail.com>
2025-12-05 19:58:26 +0530

    Fix Nest import path for tsx runtime

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import "reflect-metadata";
 import { NestFactory } from "@nestjs/core";
 import { Logger } from "@nestjs/common";
 
-import { AppModule } from "./app.module.js";
+import { AppModule } from "./app.module";
 
 async function bootstrap() {
   const logger = new Logger("Bootstrap");
